2efce4428666074d73ebdf860b5ea2277cb757ad
R-Entertainment <richarddolot66@gmail.com>
2023-12-28 13:12:32 +0800

    .

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'PressStart2P': ['"Press Start 2P"'],
+        'PixSans': ['"Pixelify Sans"'],
+      },
+    },
+  },
+  plugins: [],
+}
+
